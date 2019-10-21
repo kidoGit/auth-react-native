09acf9c2718379df7aa1f9d6c9de90a48cc0b20c
Poonamkumar Parekh <pkparekh95@gmail.com>
2019-10-21 18:48:35 +0530

    [mod9] Added loginform and input comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     UNSAFE_componentWillMount() {
-        firebase.initializeApp({
-            apiKey: "AIzaSyAEnUvhDkRJOdSD9gLusdp0M6Q1SACpXlk",
-            authDomain: "auth-react-native-be3bd.firebaseapp.com",
-            databaseURL: "https://auth-react-native-be3bd.firebaseio.com",
-            projectId: "auth-react-native-be3bd",
-            storageBucket: "auth-react-native-be3bd.appspot.com",
-            messagingSenderId: "1007241663527",
-            appId: "1:1007241663527:web:4cce032b8b71b60e66ea02",
-            measurementId: "G-4MX82KMM5G"
-        });
+        if (!firebase.apps.length) {
+            firebase.initializeApp({
+                apiKey: "AIzaSyAEnUvhDkRJOdSD9gLusdp0M6Q1SACpXlk",
+                authDomain: "auth-react-native-be3bd.firebaseapp.com",
+                databaseURL: "https://auth-react-native-be3bd.firebaseio.com",
+                projectId: "auth-react-native-be3bd",
+                storageBucket: "auth-react-native-be3bd.appspot.com",
+                messagingSenderId: "1007241663527",
+                appId: "1:1007241663527:web:4cce032b8b71b60e66ea02",
+                measurementId: "G-4MX82KMM5G"
+            });
+        }
     }
 
     render() {
         return(
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app!</Text>
+                <LoginForm />
             </View>
         );
     };
